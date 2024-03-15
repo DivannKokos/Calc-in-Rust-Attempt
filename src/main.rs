f98965f42587019b452f4a76e3a78a3cb3daf27c
DivannKokos <divann.2@yandex.ru>
2024-03-16 01:10:58 +0700

    init commit

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+
+// Calc-In-Rust-Attempt - Attempt to do simple calculator in Rust, while starting with zero knowledge of Rust.
+// Copyright (C) <2024>  <teamoor>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+extern crate native_windows_gui as nwg;
+extern crate native_windows_derive as nwd;
+
+use nwd::NwgUi;
+use nwg::NativeUi;
+
+
+#[derive(Default, NwgUi)]
+pub struct BasicApp {
+    #[nwg_control(size: (300, 115), position: (300, 300), title: "Calc in Rust Attempt", flags: "WINDOW|VISIBLE")]
+    #[nwg_events( OnWindowClose: [BasicApp::say_goodbye] )]
+    window: nwg::Window,
+
+    #[nwg_control(text: "", size: (280, 25), position: (10, 10))]
+    name_edit: nwg::TextInput,
+
+    #[nwg_control(text: "button", size: (280, 60), position: (10, 40))]
+    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
+    hello_button: nwg::Button
+}
+
+impl BasicApp {
+
+    fn say_hello(&self) {
+        // nwg::simple_message("Hello", &format!("Hello {}", self.name_edit.text()));
+    }
+    
+    fn say_goodbye(&self) {
+        // nwg::simple_message("Goodbye", &format!("Goodbye {}", self.name_edit.text()));
+        nwg::stop_thread_dispatch();
+    }
+
+}
+
+fn main() {
+    nwg::init().expect("Failed to init Native Windows GUI");
+
+    let _app = BasicApp::build_ui(Default::default()).expect("Failed to build UI");
+
+    nwg::dispatch_thread_events();
+}
