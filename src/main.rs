9c9d19ab31c9ed53525106a77c09023f6c50ad7e
DivannKokos <divann.2@yandex.ru>
2024-03-16 14:34:42 +0700

    remove everything

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 // Calc-In-Rust-Attempt - Attempt to do simple calculator in Rust, while starting with zero knowledge of Rust.
 // Copyright (C) <2024>  <teamoor>
 
@@ -15,113 +14,18 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-extern crate native_windows_gui as nwg;
-extern crate native_windows_derive as nwd;
+#![windows_subsystem = "console"]
 
-use nwd::NwgUi;
-use nwg::NativeUi;
-
-
-#[derive(Default, NwgUi)]
-pub struct BasicApp {
-    #[nwg_control(size: (260, 420), position: (300, 300), title: "Calc in Rust Attempt", flags: "WINDOW|VISIBLE")]
-    #[nwg_events( OnWindowClose: [BasicApp::say_goodbye] )]
-    window: nwg::Window,
-
-    #[nwg_control(text: "", size: (280, 25), position: (10, 10))]
-    name_edit: nwg::TextInput,
-    
-    #[nwg_control(text: "стереть", size: (60, 60), position: (70, 40))] // first is horizontal, second is vertical
-    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
-    button_clear_current_button: nwg::Button,
-
-    #[nwg_control(text: "⌫", size: (60, 60), position: (190, 40))] // first is horizontal, second is vertical
-    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
-    button_clear_button: nwg::Button,
-
-    #[nwg_control(text: "/", size: (60, 60), position: (190, 100))] // first is horizontal, second is vertical
-    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
-    button_4_button: nwg::Button,
-    
-    #[nwg_control(text: "7", size: (60, 60), position: (10, 160))] // first is horizontal, second is vertical
-    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
-    button2_1_button: nwg::Button,
-
-    #[nwg_control(text: "8", size: (60, 60), position: (70, 160))] // first is horizontal, second is vertical
-    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
-    button2_2_button: nwg::Button,
-
-    #[nwg_control(text: "9", size: (60, 60), position: (130, 160))] // first is horizontal, second is vertical
-    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
-    button2_3_button: nwg::Button,
-
-    #[nwg_control(text: "*", size: (60, 60), position: (190, 160))] // first is horizontal, second is vertical
-    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
-    button2_4_button: nwg::Button,
-    
-    #[nwg_control(text: "4", size: (60, 60), position: (10, 220))] // first is horizontal, second is vertical
-    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
-    button3_1_button: nwg::Button,
-
-    #[nwg_control(text: "5", size: (60, 60), position: (70, 220))] // first is horizontal, second is vertical
-    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
-    button3_2_button: nwg::Button,
-
-    #[nwg_control(text: "6", size: (60, 60), position: (130, 220))] // first is horizontal, second is vertical
-    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
-    button3_3_button: nwg::Button,
-
-    #[nwg_control(text: "-", size: (60, 60), position: (190, 220))] // first is horizontal, second is vertical
-    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
-    button3_4_button: nwg::Button,
-
-    #[nwg_control(text: "1", size: (60, 60), position: (10, 280))] // first is horizontal, second is vertical
-    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
-    button4_1_button: nwg::Button,
-
-    #[nwg_control(text: "2", size: (60, 60), position: (70, 280))] // first is horizontal, second is vertical
-    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
-    button4_2_button: nwg::Button,
-
-    #[nwg_control(text: "3", size: (60, 60), position: (130, 280))] // first is horizontal, second is vertical
-    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
-    button4_3_button: nwg::Button,
-    
-    #[nwg_control(text: "+", size: (60, 60), position: (190, 280))] // first is horizontal, second is vertical
-    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
-    button4_4_button: nwg::Button,
-
-    #[nwg_control(text: "0", size: (60, 60), position: (70, 340))] // first is horizontal, second is vertical
-    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
-    button5_2_button: nwg::Button,
-
-    // #[nwg_control(text: "button5_5", size: (60, 60), position: (130, 280))] // first is horizontal, second is vertical
-    // #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
-    // button5_3_button: nwg::Button,
-
-
-    #[nwg_control(text: "=", size: (60, 60), position: (190, 340))] // first is horizontal, second is vertical
-    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
-    button5_4_button: nwg::Button,
-}
-
-impl BasicApp {
-
-    fn say_hello(&self) {
-        // nwg::simple_message("Hello", &format!("Hello {}", self.name_edit.text()));
-    }
-    
-    fn say_goodbye(&self) {
-        // nwg::simple_message("Goodbye", &format!("Goodbye {}", self.name_edit.text()));
-        nwg::stop_thread_dispatch();
-    }
-
-}
+use std::io;
 
 fn main() {
-    nwg::init().expect("Failed to init Native Windows GUI");
+    println!("Type first number.");
 
-    let _app = BasicApp::build_ui(Default::default()).expect("Failed to build UI");
+    let mut first_number;
 
-    nwg::dispatch_thread_events();
+    io::stdin()
+        .read_line(&mut first_number)
+        .expect("Failed to read line");
+
+    println!("You guessed: {first_number}");
 }
